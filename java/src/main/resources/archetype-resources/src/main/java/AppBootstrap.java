2bbdc6437faaa7687978efff99a296fd49943726
juliaaano <juliaaano@gmail.com>
2019-08-01 15:32:11 +1000

    Remove expose ports and MDC from Java archetype

--- java/src/main/resources/archetype-resources/src/main/java/AppBootstrap.java
+++ java/src/main/resources/archetype-resources/src/main/java/AppBootstrap.java
@@ -2,7 +2,6 @@ package ${package};
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
 import static ${package}.AsciiBanner.asciiBanner;
 
@@ -12,7 +11,6 @@ public class AppBootstrap {
 
     public static void main(String... args) {
 
-        MDC.put("Correlation-Id", "correlation-id");
         asciiBanner("application-ascii-banner.txt").ifPresent(AsciiBanner::print);
 
         logger.info("App started...");
